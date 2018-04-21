7552cf67782286af9cd65486b7985a4c109aa106
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-21 18:57:55 -0500

    yargs

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require ('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+
+const argv = yargs.argv;
+
 var command = process.argv[2];
-// console.log('Command: ', command);
-console.log(process.argv);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if(command === 'add') {
-    console.log('adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if(command === 'list') {
-    console.log('listing all notes');
+    notes.getAll();
 } else if(command === 'read') {
-    console.log('reading note');    
+    notes.getNote(argv.title);    
 } else if(command === 'remove') {
-    console.log('removing note');        
+    notes.removeNote(argv.title);
 } else {
     console.log('command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,5 +1,24 @@
 console.log('Starting notes.js...')
 
-module.exports.add = (a, b) => {
-    return a + b;
+var addNote = (title, body) => {
+    console.log('Adding note', title, body)
 };
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+}
