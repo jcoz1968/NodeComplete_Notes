a4fca6c64a9a7bba725fc5458c680ca32ae311f9
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-21 18:23:56 -0500

    cleanup

--- app.js
+++ app.js
@@ -6,20 +6,3 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
-//var res = notes.addNote();
-//var res = notes.add(9, -2);
-//console.log(res);
-
-console.log(_.isString(true));
-console.log(_.isString('coz'));
-
-var filteredArray = _.uniq(['coz']);
-console.log(filteredArray);
-
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
-//     if (err) {
-//         console.log('Unable to write file');
-//     }
-// });
-
