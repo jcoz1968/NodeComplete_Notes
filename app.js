fa8ed964adbbe301faad2439276bf0dd81c04738
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-22 12:04:33 -0500

    refactor read command, log note command

--- app.js
+++ app.js
@@ -12,20 +12,25 @@ var command = argv._[0];
 console.log('Yargs', argv);
 
 if(command === 'add') {
-    var note = notes.addNote(argv.title, argv.body);
-    if (note) {
-        console.log(`Title: ${note.title}, Body: ${note.body}`);
-    } else {
-        console.log('Note already exists!')
-    }
-} else if(command === 'list') {
-    notes.getAll();
-} else if(command === 'read') {
-    notes.getNote(argv.title);    
-} else if(command === 'remove') {
-    var noteRemoved = notes.removeNote(argv.title);
-    var msg = noteRemoved ? 'successfully removed message' : 'note not found';
-    console.log(msg);    
-} else {
-    console.log('command not recognized');
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    notes.logNote(note);
+  } else {
+    console.log('Note already exists!')
+  }
+  } else if(command === 'list') {
+      notes.getAll();
+  } else if(command === 'read') {
+      var note = notes.getNote(argv.title);  
+      if(note) {
+        notes.logNote(note);
+      } else {
+        console.log('note not found');
+      }
+  } else if(command === 'remove') {
+      var noteRemoved = notes.removeNote(argv.title);
+      var msg = noteRemoved ? 'successfully removed message' : 'note not found';
+      console.log(msg);    
+  } else {
+      console.log('command not recognized');
 }
