e5f90d7b157616a9ef6e079a49aaf41200cc484c
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-22 12:37:35 -0500

    list all notes

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js...')
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -34,7 +32,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -51,7 +49,6 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
-    debugger; 
     console.log('--')
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
